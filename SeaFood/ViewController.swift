cf1c09b3cc9327378a034aab7371046dbf0637d6
Santosh Krishnamurthy <san.b350@gmail.com>
2023-01-26 13:34:31 -0600

    setup App to take a picture using camera and present the image on screen

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        // We have to use PHPicker framework to pick images from library
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+        } else{
+            print("camera not available on this device")
+        }
+        
+        
+        imagePicker.allowsEditing = false
     }
 
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // in the info dict, we can find original image and edited image and info on all the edits performed
+        // use KEY UIImagePickerController.InfoKey.originalImage to get original image
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            
+            imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true)
+        
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
 }
 
